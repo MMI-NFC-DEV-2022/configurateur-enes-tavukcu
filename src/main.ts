2a8f51e12cada17d6a0cd09a6ac18e2733e8d11b
enes-tavukcu <enes.tavukcu@edu.univ-fcomte.fr>
2024-03-06 09:27:59 +0100

    FORM

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import './assets/main.css'
+import './assets/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { plugin, defaultConfig } from '@formkit/vue'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(plugin, defaultConfig)
 
 app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,12 @@
+<script setup lang="ts">
+
+import FormSvg from '@/components/FormSvg.vue';
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h1 class="bg-red-500">
+    Test
+  </h1>
+  <FormSvg />
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
